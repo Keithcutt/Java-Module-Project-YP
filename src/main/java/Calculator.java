50b4edadd7afdad7b8b9e0e777323f18ae146715
Keithcutt <mayerhofnew@gmail.com>
2023-10-28 17:01:31 +0300

    Практическая работа из второго спринта v1.0.

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class Calculator {
+    static double sum = 0.0;
+
+    // Добавление товаров в список
+    static void goodsInput() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("Введите название товара.");
+            String name = sc.next(); // ?
+            double cost = 0.0;
+            while (true) {
+                System.out.println("Введите стоимость товара.");
+                if (sc.hasNextDouble()) {
+                    cost = sc.nextDouble();
+                    if (cost > 0.0) {
+                        sum += cost;
+                        break;
+                    }
+                } else {
+                    System.out.println("ОШИБКА! Вы указали некорректное значение.");
+                    sc.next();
+                }
+            }
+
+            Goods.list = Goods.list + " " + name + " " + Double.toString(cost) + "\n";
+
+            System.out.println("Товар успешно добавлен!");
+            System.out.println("Хотите ввести следующий товар или завершить?");
+            if (end()) {
+                break;
+            }
+        }
+        sc.close();
+        System.out.println("Добавленные товары:");
+        System.out.print(Goods.list);
+        System.out.println("===================");
+    }
+
+    // Расчет итоговой суммы
+    static void totalAmount(int people) {
+        sum /= people;
+        System.out.println("Итоговая сумма: " + Formatter.rounding(sum) + " " + Formatter.rubles(sum) + ".");
+    }
+
+    // Обрабатываем случай, если вводят слово ЗАВЕРШИТЬ.
+    static boolean end() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        if (input.equalsIgnoreCase("завершить")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,28 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int people = peopleNumberInput();
+        Calculator.goodsInput();
+        Calculator.totalAmount(people);
+    }
+    public static int peopleNumberInput() {
+        Scanner sc = new Scanner(System.in);
+        int peopleNumber;
+
+        while (true) {
+            System.out.println("На скольких человек необходимо разделить счёт?");
+            if (sc.hasNextInt()) {
+                peopleNumber = sc.nextInt();
+                if (peopleNumber > 1) {
+                    return (peopleNumber);
+                } else {
+                    System.out.println("ОШИБКА! Вы указали некорректное значение.");
+                }
+            } else {
+                System.out.println("Ошибка! Вы указали некорректное значение.");
+                sc.next();
+            }
+        }
     }
 }
